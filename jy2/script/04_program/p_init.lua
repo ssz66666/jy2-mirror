5807cff6c991757b92d328866e9733263479bd2e
MD <MD@dianhun.cn>
2019-06-05 10:19:09 +0800

    修复药馆奸商的问题, 修复杀何太冲后卡住的问题, 尝试修复战败后黑屏的问题(可能没彻底解决),修复编辑器更新带来的showname丢失问题

--- jy2/script/04_program/p_init.lua
+++ jy2/script/04_program/p_init.lua
@@ -14,7 +14,16 @@ t['初始化'] = function()
 	G.call("战斗系统_初始化",0x56061001)
 	G.start_program('通用_UI交互响应')
 	G.call("通用_显示开始菜单")
-	G.addUI('v_info');
+	-- G.addUI('v_info');
+	-- G.call("通用_关闭开始菜单")
+	-- local o_base_游戏数据 = G.QueryName(0x10050001)
+	-- local o_role_主角 = o_base_游戏数据.主角
+	-- o_role_主角.生命 = 9999
+	-- o_role_主角.力道 = 9999
+	-- o_role_主角.身法 = 9999
+	-- o_role_主角.拳掌 = 9999
+	-- G.call("通用_变更物品数量", G.QueryName(0x1006002b), 1)
+	-- G.call("场景系统_切换剧本",{n = 0, "剧情_黑木崖"})
 	-- G.call("剧情_序")
 	-- G.call("剧情_衡阳")
 	-- G.call("剧情_主地图")
